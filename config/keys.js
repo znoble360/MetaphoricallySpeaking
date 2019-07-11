2d9c80fa719d8304b1b7ce01df3c789cc9175518
travish23 <47612073+travish23@users.noreply.github.com>
2019-07-11 12:59:32 -0400

    a

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
 
-    MongoURI: 'mongodb+srv://travish23:Soccer11@mean-cwizf.mongodb.net/test?retryWrites=true&w=majority',
+    MongoURI: 'mongodb+srv://travish23:Metaphor1@mean-cwizf.mongodb.net/test?retryWrites=true&w=majority',
     secret: ''
 }
