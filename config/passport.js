384193059dd14874082986f964ed2b8ce96f514c
travish23 <47612073+travish23@users.noreply.github.com>
2019-07-14 13:45:07 -0400

    added passport and auth js file

---
+++ config/passport.js
@@ -0,0 +1,50 @@
+const LocalStrategy = require('passport-local').Strategy;
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+// Load user model
+
+const User = require('../models/user');
+
+module.exports  = function(passport){
+
+    passport.use(
+
+        new LocalStrategy({ usernameField: 'email' }, (email, password, done)=>{
+            //match user
+
+            User.findOne({ email : email })
+
+            .then(user =>{
+                if(!user){
+                    return done(null, false, {message: 'email not registered'});
+                }
+
+                //match password
+                bcrypt.compare(password, user.password, (err, isMatch)=>{
+
+                    if(err)throw err;
+                    if(isMatch){
+                        return done(null, user);
+                    }
+                    else{
+                        return done(null, false, {message: 'incorrect password'});
+                    }
+
+                });
+            });
+            //.catch(err=> console.log(err));
+        })
+    );
+
+    passport.serializeUser(function(user, done) {
+        done(null, user.id);
+      });
+      
+      passport.deserializeUser(function(id, done) {
+        User.findById(id, function(err, user) {
+          done(err, user);
+        });
+      });
+
+};
