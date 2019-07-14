cb78f00835383e6237f47fd482349cac605b3bf9
travish23 <47612073+travish23@users.noreply.github.com>
2019-07-14 14:02:23 -0400

    update app file

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const mongooose = require('mongoose');
-
+const flash = require('connect-flash');
+const session = require('express-session');
 const db = require('./config/keys').MongoURI;
-
+const passport = require('passport');
 const app = express();
+//pasport config
 
+require('./config/passport')(passport);
 //connect to mongo
 mongooose.connect(db, {useNewUrlParser: true})
 .then(()=>console.log("Mongo db connected"))
@@ -16,13 +19,38 @@ app.use(expressLayouts);
 app.set('view engine', 'ejs');
 
 // Body parser Middleware
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({extended: true}));
 
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
 
+//Express Session
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+
+//passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+//connect flash
+app.use(flash());
+
+//global variables
+app.use((req,res,next)=>{
+
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+});
+
 //routes
 app.use('/', require('./routes/index'));
 app.use('/users', require('./routes/users'));
+
