11c1900210c069c92b6504804873f3461862cc5b
millerkalvin <Millerkalvin@gmail.com>
2019-07-18 16:20:57 -0400

    Update metaphor.js

--- models/metaphor.js
+++ models/metaphor.js
@@ -1,17 +1,9 @@
-const mongoose = require("mongoose");
-const MetaphorSchema = new mongoose.Schema({
-	
-	content: {
-		type: String,
-		required: true
-	},
-	
-	description: {
-		type: String,
-		required: true
-	}
-	
-});
+const mongoose = require('mongoose');
+// You can make a model to represent a metaphor
+let MetaphorSchema = new mongoose.Schema( {
+  text: { type: String },
+  explanation: { type: String }
+} );
 
-const Metaphor = mongoose.model('Metaphor', MetaphorSchema);
-module.exports = Metaphor;
+const MetaphorModel = mongoose.model('Metaphor', MetaphorSchema)
+module.exports = MetaphorModel;
