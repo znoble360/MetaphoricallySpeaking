49834e1a93821635a9427ee1d98a764db751865a
jakeguelli <51209694+jakeguelli@users.noreply.github.com>
2019-07-24 19:54:13 -0400

    changed metaphor-template to be dynamic; metaphor-template.ejs will create an list of metaphor cards based on the array passed to it;  hard coded in two metaphors when rendering welcome page for testing purposes; minor style changes;

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { ensureAuthenticated } = require('../config/auth');
 //welcome page
-router.get('/', (req,res)=> res.render("welcome"));
+router.get('/', (req,res)=> res.render("welcome", {metaphor : [{text : "It's raining cats and dogs.", explanation : "It is raining hard.", author : "metaphor_guy", num_likes : "130", num_dislikes : "14", time_string : "3 days ago"}, {text : "Cry me a river.", explanation : "You are overreacting.", author : "metaknight1337", num_likes : "2043", num_dislikes : "50", time_string : "12/2/18 at 11:30"}]}));
 
 
 router.get('/dashboard',(ensureAuthenticated), (req,res)=> 
