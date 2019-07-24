c8713c4b3564ea456367f22668c2e007babeff98
jakeguelli <51209694+jakeguelli@users.noreply.github.com>
2019-07-24 14:40:51 -0400

    changed logout redirect to welcome.ejs; created template for navbar and included it on every page; created [in progress] metaphor template; rearranged welcome page slightly

--- routes/users.js
+++ routes/users.js
@@ -113,7 +113,7 @@ router.post('/login', (req,res,next)=>{
 router.get('/logout', (req, res) => {
   req.logout();
   req.flash('success_msg', 'You are logged out');
-  res.redirect('/users/login');
+  res.redirect('/');
 });
 
 module.exports = router;
