bb05a1c739f5b3ec0e3d4aec30e4c15f180e3122
jakeguelli <51209694+jakeguelli@users.noreply.github.com>
2019-07-24 21:01:08 -0400

    dashboard overhaul; added modal (confirmation dialogue box) for logout button; added navbar dropdown menu for user settings etc.; hard coded in sample metaphors when rendering dashboard via index.js

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ router.get('/', (req,res)=> res.render("welcome", {metaphor : [{text : "It's rai
 router.get('/dashboard',(ensureAuthenticated), (req,res)=> 
 
 res.render("dashboard", {
-    name: req.user.name
+    name: req.user.name, metaphor : [{text : "It's raining cats and dogs.", explanation : "It is raining hard.", author : "metaphor_guy", num_likes : "130", num_dislikes : "14", time_string : "3 days ago"}, {text : "Cry me a river.", explanation : "You are overreacting.", author : "metaknight1337", num_likes : "2043", num_dislikes : "50", time_string : "12/2/18 at 11:30"}]
 }));
 
 
