c15242e76ae4fd7ee9078ba84d658fbcd6b16221
travish23 <47612073+travish23@users.noreply.github.com>
2019-07-25 14:26:02 -0400

    update metaphor schema

--- models/metaphor.js
+++ models/metaphor.js
@@ -5,7 +5,10 @@ let MetaphorSchema = new mongoose.Schema( {
   text: { type: String },
   explanation: { type: String },
   likedBy: { type: [mongoose.Schema.Types.ObjectId] },
-  dislikedBy: { type: [mongoose.Schema.Types.ObjectId] }
+  dislikedBy: { type: [mongoose.Schema.Types.ObjectId] },
+  likeCount: {type: Number },
+  dislikeCount: { type: Number },
+  date: { type: Date }
 } );
 
 const MetaphorModel = mongoose.model('Metaphor', MetaphorSchema)
