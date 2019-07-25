e25bdfa1c87e287e51aef0cd3d01322261cf7269
travish23 <47612073+travish23@users.noreply.github.com>
2019-07-25 18:26:26 -0400

    added display all metaphors

--- routes/display.js
+++ routes/display.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const mongooose = require('mongoose');
 
+//const db = require('../config/keys').MongoURI;
 //gets metaphor model
 const Metaphor = require('../models/metaphor');
 
 
-router.get('/display',(req,res)=>{
-
-
-res.json("working on display");
-
+router.get('/display',(req,res,next)=>{
+    var resultArray = [];
+    Metaphor.find({}).exec(function (err, documents){
+        if (err)throw err;
+        else{ 
+           res.send(documents);
+    }
 });
+});
+
+module.exports = router;
