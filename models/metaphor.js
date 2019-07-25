3698d7bfab6e3bf66fc8a94f9e0b730a22898ade
hwbc1989 <51303679+hwbc1989@users.noreply.github.com>
2019-07-25 00:28:31 -0400

    Update metaphor.js

--- models/metaphor.js
+++ models/metaphor.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose');
+
 // You can make a model to represent a metaphor
 let MetaphorSchema = new mongoose.Schema( {
   text: { type: String },
-  explanation: { type: String }
-});
+  explanation: { type: String },
+  likedBy: { type: [mongoose.Schema.Types.ObjectId] },
+  dislikedBy: { type: [mongoose.Schema.Types.ObjectId] }
+} );
 
 const MetaphorModel = mongoose.model('Metaphor', MetaphorSchema)
 module.exports = MetaphorModel;
