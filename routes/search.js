312cb625ab5d2aef2825ed6e5fef45159d393d7a
Jake <jakeguelli@gmail.com>
2019-07-28 18:31:32 -0400

    made significant progress on search when logged in and not logged in

--- routes/search.js
+++ routes/search.js
@@ -4,6 +4,7 @@
 
 const express = require('express');
 const router = express.Router();
+const { ensureAuthenticated } = require('../config/auth');
 
 //gets metaphor model
 const Metaphor = require('../models/metaphor');
@@ -32,7 +33,66 @@ var search = function (request, response){
 		response.send({searchResults : returnArray});
 	});
 	
+<<<<<<< Updated upstream
 
+=======
+}
+
+//A new and improved search function that should work in O(1) time, 
+//because it directly queries what we need.
+
+//It returns the results sorted by likes in descending order, and can also 
+//find a metaphor by unique ID
+
+betterSearch = function (request, response){
+	//gets string from request
+	const searchString = request.query.searchString;
+	
+	var returnArray = [];
+	
+	//uses a regular expression shortcut to query exactly what we need, and saves it in an array
+	//also specifies that the result must be sorted by likeCount
+	Metaphor.find({text: {$regex: searchString, $options: "$i", } }).sort({likeCount : -1}).exec(function (err, documents){
+		
+		//sets the result of the query to a return variable
+
+		returnArray.push(documents);
+		
+		//if a post ID was input, it will find that and push it onto the return array
+		Metaphor.find({_id: searchString}).exec(function (err, documents2){
+			if(documents2 != null){
+				returnArray.push(documents2);
+			}
+
+			console.log("test");
+
+			//sends a JSON object with the array of results, labeled searchResults.
+
+			
+
+			if (request.isAuthenticated()) {
+				response.render("searchresults", {
+					page: "searchresults",
+					search: searchString,
+					name: request.user.name,
+					id: request.user._id,
+					email: request.user.email,
+					metaphor: returnArray
+				});
+			} else {
+				response.render("searchresults", {
+					page: "searchresults",
+					search: searchString,
+					name: null,
+					id: null,
+					email: null,
+					metaphor: returnArray
+				});
+			}
+		
+		});
+	});
+>>>>>>> Stashed changes
 }
 
 
