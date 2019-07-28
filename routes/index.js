1631cd15bc3b89e292683b5cb837fd743df6aae3
jakeguelli <51209694+jakeguelli@users.noreply.github.com>
2019-07-28 02:13:36 -0400

    many many things

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,87 @@
 const express = require('express');
 const router = express.Router();
 const { ensureAuthenticated } = require('../config/auth');
+const temp = "5d2e39fbd4ffb0000462dfcd";
+
+//temporary variables for testing
+const metaphor1 = {
+    id: "000",
+    text: "It's raining cats and dogs.",
+    explanation: "It is raining hard.",
+    author: "metaphor_guy",
+    authorID: "xxx",
+    likeCount: "130",
+    dislikeCount: "14",
+    time_string: "3 days ago"
+}
+
+const metaphor2 = {
+    id: "111",
+    text: "Cry me a river.",
+    explanation: "You are overreacting.",
+    author: "metaknight1337",
+    authorID: temp,
+    dislikeCount: "50",
+    time_string: "12/2/18 at 11:30"
+}
+
+const metaphors = [metaphor1, metaphor2];
+const metaphors2 = [];
+
 //welcome page
-router.get('/', (req,res)=> res.render("welcome", {metaphor : 
-    [{text : "It's raining cats and dogs.", explanation : "It is raining hard.", 
-    author : "metaphor_guy", likeCount : "130", dislikeCount : "14", time_string : "3 days ago"}, 
-    {text : "Cry me a river.", explanation : "You are overreacting.", author : "metaknight1337", 
-    likeCount : "2043", dislikeCount : "50", time_string : "12/2/18 at 11:30"}]}));
+router.get('/', (req,res)=> res.render("welcome", {
+    page: "welcome",
+    id: null,
+    metaphor: metaphors
+}));
+
+router.get('/please-log-in', (req,res)=> {
+    req.flash('success_msg', 'You must be logged in to do that.');
+    res.redirect("/");
+});
 
 
 router.get('/dashboard',(ensureAuthenticated), (req,res)=> 
 
 res.render("dashboard", {
+    page: "dashboard",
     name: req.user.name,
     id: req.user._id,
-    metaphor : [{text : "It's raining cats and dogs.", 
-    explanation : "It is raining hard.", author : "metaphor_guy", 
-    likeCount : "130", dislikeCount : "14", time_string : "3 days ago"}, 
-    {text : "Cry me a river.", explanation : "You are overreacting.", 
-    author : "metaknight1337", likeCount : "2043", dislikeCount : "50", 
-    time_string : "12/2/18 at 11:30"}]
+    email: req.user.email,
+    metaphor: metaphors
 }));
 
+router.get('/myprofile', (ensureAuthenticated), (req,res)=>
+
+res.render("myprofile", {
+    page: "myprofile",
+    name: req.user.name,
+    id: req.user._id,
+    email: req.user.email,
+    metaphor: metaphors,
+    display: "posts"
+}));
+
+router.get('/myprofile/likes', (ensureAuthenticated), (req,res)=>
+
+res.render("myprofile", {
+    page: "myprofile",
+    name: req.user.name,
+    id: req.user._id,
+    email: req.user.email,
+    metaphor: metaphors,
+    display: "likes"
+}));
+
+router.get('/searchresults', (ensureAuthenticated), (req,res)=>
+
+res.render("searchresults", {
+    page: "searchresults",
+    name: req.user.name,
+    id: req.user._id,
+    email: req.user.email,
+    metaphor: metaphors
+}));
 
 
 
