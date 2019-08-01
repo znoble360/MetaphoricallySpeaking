9bb0ce23257223fe397d838558958a65d9165233
jakeguelli <51209694+jakeguelli@users.noreply.github.com>
2019-07-31 22:06:20 -0400

    Update reports.js

--- models/reports.js
+++ models/reports.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 
 let ReportSchema = new mongoose.Schema( {
     issue: { type: String },
-    metaphorID: { type: [mongoose.Schema.Types.ObjectId], ref: 'Metaphor' },
-    authorID: {type: [mongoose.Schema.Types.ObjectId], ref: 'User'},
-    userID: {type: [mongoose.Schema.Types.ObjectId], ref: 'User'}
+    metaphorID: { type: mongoose.Schema.Types.ObjectId, ref: 'Metaphor' },
+    authorID: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+    userID: {type: mongoose.Schema.Types.ObjectId, ref: 'User'}
   } );
   
   const ReportModel = mongoose.model('Report', ReportSchema);
