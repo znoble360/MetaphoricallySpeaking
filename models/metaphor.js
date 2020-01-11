c4af00e94bb296a386b1d9b9a942b561596a9314
travish23 <47612073+travish23@users.noreply.github.com>
2020-01-11 12:57:37 -0500

    push

--- models/metaphor.js
+++ models/metaphor.js
@@ -9,13 +9,9 @@ let MetaphorSchema = new mongoose.Schema( {
   likeCount: {type: Number },
   dislikeCount: { type: Number },
   time : { type : Date, default: Date.now },
-<<<<<<< HEAD
   authorID: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
   author: {type: mongoose.Schema.Types.String, ref: 'User'},
   report: {type: [String]}
-=======
-  authorID: {type: mongoose.Schema.Types.ObjectId, ref: 'User'}
->>>>>>> 9bb0ce23257223fe397d838558958a65d9165233
 } );
 
 const MetaphorModel = mongoose.model('Metaphor', MetaphorSchema);
